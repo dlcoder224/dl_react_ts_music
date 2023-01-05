39c8a45b7763c4c5e5570f98195a3464dec014fa
liuwenlong <liuwenlong@111.com>
2023-01-05 23:41:41 +0800

    ✨ feat: 使用axios

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,12 +1,32 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import etcRequest from '@/service'
 
 interface IProps {
   children?: ReactNode
 }
 
 const Recommend: FC<IProps> = () => {
-  return <div> Recommend</div>
+  const [banners, setBanners] = useState<any[]>([])
+
+  useEffect(() => {
+    etcRequest
+      .get({
+        url: '/banner'
+      })
+      .then((res) => {
+        console.log(res)
+        setBanners(res.banners)
+      })
+  }, [])
+
+  return (
+    <div>
+      {banners.map((item, index) => {
+        return <div key={index}>{item.imageUrl}</div>
+      })}
+    </div>
+  )
 }
 
 export default memo(Recommend)
